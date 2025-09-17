184aba657ae52acd36a15dbbe06e4b13833e33d6
fsochaty <pervakov.2020@inbox.ru>
2025-09-17 22:32:54 +0300

    сделал navbar, но только набросок

--- summaries/news/urls.py
+++ summaries/news/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [
-    path("", views.MainPage.as_view(), name='MainPage')
+urlpatterns = [    
+    path("", views.MainPage.as_view(), name='MainPage'),
+    path('/popular-news',views.PopularNews.as_view(), name='popular_news' ),
+    path('/today-news', views.TodayNews.as_view(), name="today_news"),
+    path('/yestarday-news', views.YestardayNews.as_view(), name="yestarday_news"),
+    path('/search-news', views.SearcNews.as_view(), name='search_news')
 ]
--- summaries/news/views.py
+++ summaries/news/views.py
@@ -9,3 +9,30 @@ class MainPage(View):
             'pages-html/main.html'
         )
 
+class PopularNews(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'pages-html/popular-news.html'
+        )
+    
+class YestardayNews(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            'pages-html/yestarday-news.html'
+        )    
+    
+class TodayNews(View):
+    def get(self,request, *args ,**kwargs):
+        return render(
+            request,
+            'pages-html/today-news.html'
+        )    
+ 
+class SearcNews(View):
+    def get(self,request, *args ,**kwargs):
+        return render(
+            request,
+            'pages-html/searchPage.html'
+        )       
