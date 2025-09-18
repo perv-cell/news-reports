faab3113d6b4a72883fb1b67dc5f082d23efb22e
fsochaty <pervakov.2020@inbox.ru>
2025-09-18 15:56:06 +0300

    добавлены новые страницы, начал делать footer

--- summaries/news/urls.py
+++ summaries/news/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 urlpatterns = [    
     path("", views.MainPage.as_view(), name='MainPage'),
-    path('/popular-news',views.PopularNews.as_view(), name='popular_news' ),
-    path('/today-news', views.TodayNews.as_view(), name="today_news"),
-    path('/yestarday-news', views.YestardayNews.as_view(), name="yestarday_news"),
-    path('/search-news', views.SearcNews.as_view(), name='search_news')
+    path('popular-news',views.PopularNews.as_view(), name='popular_news' ),
+    path('today-news', views.TodayNews.as_view(), name="today_news"),
+    path('yestarday-news', views.YestardayNews.as_view(), name="yestarday_news"),
+    path('technological-news', views.TechnologicalNews.as_view(), name='tetechnological_news'),
+    path('political-news', views.PoliticalNews.as_view(), name='political_news'),
+    path('sports-news', views.SportsNews.as_view(), name='sports_news'),
+    path('search-news', views.SearcNews.as_view(), name='search_news'),
+    path('save-news', views.SaveNews.as_view(), name='save_news'),
 ]
--- summaries/news/views.py
+++ summaries/news/views.py
@@ -35,4 +35,32 @@ class SearcNews(View):
         return render(
             request,
             'pages-html/searchPage.html'
-        )       
+        )
+
+class PoliticalNews(View):
+    def get(self,request, *args ,**kwargs):
+        return render(
+            request,
+            'pages-html/political-news.html'
+        )
+
+class TechnologicalNews(View):
+    def get(self,request, *args ,**kwargs):
+        return render(
+            request,
+            'pages-html/technologies.html'
+        )
+    
+class SportsNews(View):
+    def get(self,request, *args ,**kwargs):
+        return render(
+            request,
+            'pages-html/sports.html'
+        ) 
+
+class SaveNews(View):
+    def get(self,request, *args ,**kwargs):
+        return render(
+            request,
+            'pages-html/save-news.html'
+        )        
